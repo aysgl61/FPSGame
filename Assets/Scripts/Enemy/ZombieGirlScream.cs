795e6f8d0e65194aeae50c6c86b9f325817d7fb1
aysgl61 <aysegulaltun6161@gmail.com>
2023-07-14 14:59:53 +0300

    punch fonksiyonunu sildim

--- Assets/Scripts/Enemy/ZombieGirlScream.cs
+++ Assets/Scripts/Enemy/ZombieGirlScream.cs
@@ -7,25 +7,17 @@ public class ZombieGirlScream : MonoBehaviour
     AudioSource zombieGirlAS;
 
     public AudioClip screamAC;
-    public AudioClip punchAC;
+    
     void Start()
     {
         zombieGirlAS = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void Scream()
     {
         zombieGirlAS.PlayOneShot(screamAC);
     }
 
-    public void Punch()
-    {
-        zombieGirlAS.PlayOneShot(punchAC);
-    }
+   
 }
