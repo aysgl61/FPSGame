842e505a01145b6ebd13bf917ec840600d01cbcf
aysgl61 <aysegulaltun6161@gmail.com>
2023-12-17 20:12:18 +0300

    değişiklik.

--- Assets/Scripts/Enemy/ZombieGirlScream.cs
+++ Assets/Scripts/Enemy/ZombieGirlScream.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,13 +7,23 @@ public class ZombieGirlScream : MonoBehaviour
     AudioSource zombieGirlAS;
 
     public AudioClip screamAC;
+    Transform target; //Ana karakterin konumunu tutuyo
     
     void Start()
     {
         zombieGirlAS = GetComponent<AudioSource>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
+    private void Update()
+    {
+        float distance = Vector3.Distance(transform.position, target.position); //zombinin ve ana karakterin arasındaki mesafe
+        if(distance < 2)
+        {
+            Scream();
+        }
 
+    }
     public void Scream()
     {
         zombieGirlAS.PlayOneShot(screamAC);
