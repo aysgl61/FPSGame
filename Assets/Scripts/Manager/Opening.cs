4850747549b0eb77106f39274635026b38080d76
aysgl61 <aysegulaltun6161@gmail.com>
2023-12-19 12:51:15 +0300

    fade secrren yapıldı

---
+++ Assets/Scripts/Manager/Opening.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson; // first person controller'a erişmek için ekledik
+using UnityEngine.UI;
+public class Opening : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject fadeScreen;
+    public GameObject text; //alt yazı için
+
+    private void Start()
+    {
+        player.GetComponent<FirstPersonController>().enabled = true; //oyunun başında karakter hareket edemesin
+        StartCoroutine(ScenePlayer());
+    }
+
+    IEnumerator ScenePlayer()
+    {
+        yield return new WaitForSeconds(1.5f);
+        fadeScreen.SetActive(false);
+        text.GetComponent<Text>().text = "Aman Yarabbi neredeyim ben!! ";
+        yield return new WaitForSeconds(2f);
+        text.GetComponent<Text>().text = "";
+        player.GetComponent<FirstPersonController>().enabled = true;
+    }
+ }
