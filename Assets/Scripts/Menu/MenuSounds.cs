10fba31a43b77276c06a82ae2ea3f77777c5df12
aysgl61 <aysegulaltun6161@gmail.com>
2024-02-20 16:04:04 +0300

    .

---
+++ Assets/Scripts/Menu/MenuSounds.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSounds : MonoBehaviour
+{
+    public AudioSource fx; //sesleri çalabilmek için audiosource oluşturdum
+    public AudioClip hoverFx; //butonun üzerine gelince ses çıkaracak
+    public AudioClip clickFx; //butona tıklayınca ses çıkaracak
+
+    public void HoverSound()
+    {
+        fx.PlayOneShot(hoverFx);
+    }
+
+    public void ClickSound()
+    {
+        fx.PlayOneShot(clickFx);
+    }
+}
